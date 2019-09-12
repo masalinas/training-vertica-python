df64eaeff0bf87d2679ac7df09fa6abf2283cd50
Miguel Salinas Gancedo <miguel@thingtrack.com>
2019-09-12 10:49:35 +0200

    last update

---
+++ samples/vertica-sample.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# ------- file: myplot.py ------
+import vertica_python
+import logging
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# ------- create vertica connection ------
+conn_info = {'host': '192.168.1.16',
+             'port': 5433,
+             'user': 'dbadmin',
+             'password': 'password',
+             'database': 'VMart',
+             'log_level': logging.INFO,
+             'log_path': 'training-vertica-python.log'}
+
+with vertica_python.connect(**conn_info) as connection:
+	cur = connection.cursor()
+
+	cur.execute("SELECT sa.Deptno, sa.Empno, sa.Sal, ROUND(AVG(sa.Sal) OVER(PARTITION BY sa.Deptno), 2) AS Avg FROM training.salary sa")
+	#cur.execute("SELECT sa.Deptno, sa.Empno, sa.Sal, ROUND(AVG(sa.Sal) OVER(PARTITION BY sa.Deptno), 2) AS Avg FROM training.salary sa")
+
+	# list all cursor registers
+	#for row in cur.iterate():
+		#print(row)
+
+    # convert source to matplotlib 
+	data = np.array(cur.fetchall())
+
+	# get plot columns
+	EmpNo = data[:,1]
+	Sal = data[:,2].astype(np.float)
+	AVGSal = data[:,3].astype(np.float)
+
+	# plot data
+	plt.plot(EmpNo, Sal, 'bo-', label="Salary")
+	plt.plot(EmpNo, AVGSal, 'go-', label="AVG Salary")
+
+	for i, j in zip(EmpNo, Sal):
+		plt.text(i, j, str(j))
+
+	for i, j in zip(EmpNo, AVGSal):
+		plt.text(i, j, str(j))
+
+	# Place a legend to the right of this smaller subplot.
+	plt.legend(loc='upper left')
+	plt.title('AVG Analytics')
+
+	plt.show()
